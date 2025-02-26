fb1564f787bb3e860fd0b21ae0cbceb2b4497efb
Nikhila Baby <128206204+nikhi-la@users.noreply.github.com>
2025-02-26 22:01:48 +0530

    Add files via upload

---
+++ Code/Controllers/Section1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorExample.Controllers //Use project name instead of RazorExample
+{
+    public class Section1Controller : Controller //Use Controller name instead of Section1Controller
+    {
+        // GET: Section1
+        public ActionResult section1() //Use corresponding .cshtml filename instad of section1
+        {
+            return View();
+        }
+    }
+}
---
+++ Code/Controllers/Section2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorExample.Controllers //Use project name instead of RazorExample
+{
+    public class Section2Controller : Controller //Use Controller name instead of Section2Controller
+    {
+        // GET: Section2
+        public ActionResult section2() //Use corresponding .cshtml filename instad of section2
+        {
+            return View();
+        }
+    }
+}
